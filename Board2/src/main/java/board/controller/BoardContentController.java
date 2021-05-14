a3c7971d8ef4049727722efdade41824038a07e2
LSY <lsysysy91@gmail.com>
2021-05-14 11:16:29 +0900

    board_mybatis_content&writeform&controllers

---
+++ Board2/src/main/java/board/controller/BoardContentController.java
@@ -0,0 +1,37 @@
+package board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import board.model.BoardBean;
+import board.model.BoardDao;
+
+@Controller
+public class BoardContentController {
+	
+	private final String command = "/content.bd";
+	private final String getPage = "content";
+	
+	@Autowired
+	private BoardDao bDao;
+	
+	@RequestMapping(value=command, method=RequestMethod.GET)
+	public ModelAndView content(
+			@RequestParam(value="num", required=true) int num,
+			@RequestParam(value="pageNumber", required=true) int pageNumber) {
+		
+		bDao.upCount(num);
+		
+		BoardBean board = bDao.getData(num);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("board", board);
+		mav.addObject("pageNumber", pageNumber);
+		mav.setViewName(getPage);
+		return mav;
+	}
+}
